b5c14e83222e963325c7d7ea531503830eddce20
Kristian Kubik <kristian.kubik@gmail.com>
2015-04-05 20:00:40 +0200

    remove my native language comments & messages

--- plex-fix-opensubtitles.sh
+++ plex-fix-opensubtitles.sh
@@ -4,17 +4,17 @@ USER='root'
 PLEX_PATH="/opt/plex"
 MEDIA_PATH="Library/ApplicationSupport/Plex Media Server/Media/localhost/"
 
-# skontrolujeme uzivatela
+# check user
 if [ `whoami` != $USER ]; then
 	if id -u $USER >/dev/null 2>&1; then
 		if su $USER; then
 			echo "login success!"
 		else 
-			echo "neplatny login"
+			echo "failed"
 			exit 0
 		fi
 	else
-		echo "User $USER neexistuje!"
+		echo "User $USER does not exist!"
 		exit 0
 	fi
 fi
@@ -22,7 +22,6 @@ fi
 cd "$PLEX_PATH/$MEDIA_PATH"
 
 find . -name '*.srt' -path '*/cs/*' -type f | while read line; do
-	#echo "$line"
 	SUB_FILE=$(basename "$line")
 	if [ -f "$line.bak" ]; then
 		echo "- Skipping $SUB_FILE"
